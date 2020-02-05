00874c53fdc6edaa7ec64e6bfd7b7f5f5ff791af
FedericoPosada2 <fedeposa122@gmail.com>
2020-02-05 16:12:09 -0500

    Commit

--- solution.rb
+++ solution.rb
@@ -1,13 +1,5 @@
 require 'sinatra'
 
 get '/' do
-file="<h1>Hola</h1>"
-	50.times do |i|
-		if (i+1)%2 == 0
-		file+="\n <h3> #{i+1} Soy Par!"
-		else
-		file+="\n <h3> #{i+1} Soy Impar!"
-		end
-	end
-return file
+	erb	:index
 end
